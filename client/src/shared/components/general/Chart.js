880c7f54f8ecf83936b743fc2abd25c7648b2f97
legenddaniel <siyuanincanada@gmail.com>
2020-08-31 23:27:08 -0400

    add chart & selector func

--- client/src/shared/components/general/Chart.js
+++ client/src/shared/components/general/Chart.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer, ReferenceLine } from 'recharts';
+import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer, ReferenceLine, ReferenceDot } from 'recharts';
 
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -15,32 +15,116 @@ const useStyles = makeStyles({
   }
 });
 
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const formatter = (value, ...params) => [`$${value.toLocaleString()}`, ...params];
+
+const initChart = (month, year) => {
+  const date = new Date();
+  const numOfDays = new Date(year, month + 1, 0).getDate();
+  const today = date.getDate();
+  const dateToday = new Date().toDateString().slice(4, 10);
+  const thisMonth = date.getMonth();
+  const thisYear = date.getFullYear();
+
+  // Future: data end at 1st day, reference at 1st day
+  // Current: data end at today, reference at today
+  // Past: data end at the last day, reference at the last day
+  const chartConfig = [
+    {
+      date: 'current',
+      condition: month === thisMonth && year === thisYear,
+      total: d => d <= today ? 0 : null,
+      reference: dateToday
+    },
+    {
+      date: 'past',
+      condition: year < thisYear || (year === thisYear && month < thisMonth),
+      total: 0,
+      reference: `${months[month]} ${numOfDays}`
+    },
+    {
+      date: 'future',
+      condition: year > thisYear || (year === thisYear && month > thisMonth),
+      total: null,
+      reference: `${months[month]} 01`
+    }
+  ];
+
+  for (let config of chartConfig) {
+    const { condition } = config;
+    if (condition) {
+      const { total, reference } = config;
+      const receipts = [{ _id: `${months[month]} 01`, total: 0 }];
+      for (let d = 2; d <= numOfDays; d++) {
+        receipts.push({
+          _id: `${months[month]} ${d > 9 ? '' : '0'}${d}`,
+          total: total instanceof Function ? total(d) : total
+        });
+      }
+      return { receipts, reference };
+    }
+  }
+}
+
 export function Chart(props) {
   const classes = useStyles();
-  const [monthlyReceipts, setMonthlyReceipts] = useState([]);
-  const [total, setTotal] = useState('--');
-  const [lastDay, setLastDay] = useState('');
+  const { data, year, month } = props;
+  const { receipts, reference } = initChart(month, year);
 
-  const { data } = props;
+  const getReceipt = (source, ref) => source.find(receipt => receipt._id === ref);
+
+  const [monthlyReceipts, setMonthlyReceipts] = useState(receipts);
+  const [total, setTotal] = useState(0);
+  const [latestValue, setlatestValue] = useState(getReceipt(receipts, reference).total);
 
   useEffect(() => {
     const total = data.reduce((a, b) => a + b.total, 0);
-    const lastDay = data[0] && data[0]._id;
 
-    setTotal(total || '--');
-    setLastDay(lastDay);
-    setMonthlyReceipts(data);
+    setTotal(total);
+    setMonthlyReceipts(receipts => {
+      const mergedReceipts = receipts.map(receipt => {
+        return getReceipt(data, receipt._id) || receipt;
+      });
+      return mergedReceipts;
+    });
   }, [data])
 
-  const formatter = (value, ...params) => [`$${value.toLocaleString()}`, ...params];
+  useEffect(() => {
+    setMonthlyReceipts(receipts);
+  }, [month, year])
+
+  useEffect(() => {
+    const latestValue = getReceipt(monthlyReceipts, reference).total;
+    setlatestValue(latestValue);
+  }, [monthlyReceipts])
 
   const renderLineChart = (
     <ResponsiveContainer height={120} debounce={100}>
       <LineChart data={monthlyReceipts}>
-        <XAxis dataKey="_id" reversed axisLine={{ stroke: "#f0f2fa", strokeWidth: 2 }} tickLine={false} padding={{ left: 20, right: 20 }} />
-        {/* <YAxis /> */}
-        <Line type="monotone" dataKey="total" stroke="#4366a7" strokeWidth={2} dot={false} activeDot={{ stroke: '#fff', strokeWidth: 3, r: 6 }} />
-        <ReferenceLine x={lastDay} stroke="#f0f2fa" />
+        <XAxis
+          dataKey="_id"
+          interval="preserveStartEnd"
+          axisLine={{ stroke: "#f0f2fa", strokeWidth: 2 }}
+          tickLine={false}
+          padding={{ left: 20, right: 20 }}
+        />
+        <Line
+          type="monotone"
+          dataKey="total"
+          stroke="#4366a7"
+          strokeWidth={2}
+          dot={false}
+          activeDot={{ stroke: '#fff', strokeWidth: 3, r: 6 }}
+        />
+        <ReferenceLine x={reference} stroke="#f0f2fa" />
+        <ReferenceDot
+          x={reference}
+          y={latestValue}
+          r={5}
+          isFront
+          fill="#38cc89"
+          stroke="none"
+        />
         <Tooltip separator=": " formatter={formatter} />
       </LineChart>
     </ResponsiveContainer>
--- server/routes/users-routes.js
+++ server/routes/users-routes.js
@@ -38,7 +38,7 @@ router.get(
 
 router.get('/topcategories/:userid', usersController.getTopCategories);
 
-router.get('/monthlytransactions/:userid&:month&:timezone', 
+router.get('/monthlytransactions/:userid&:year&:month&:timezone', 
 chartController.getMonthlyTransactions);
 router.get('/receipts/export/:month', usersController.exportReceipts);
 
