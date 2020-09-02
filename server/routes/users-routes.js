770e383417ff9896925271bbe1e3516d6f295a7a
legenddaniel <53063815+legenddaniel@users.noreply.github.com>
2020-09-02 17:32:08 -0400

    Fix typo

--- server/routes/users-routes.js
+++ server/routes/users-routes.js
@@ -38,7 +38,7 @@ router.get(
 
 router.get('/topcategories/:userid', usersController.getTopCategories);
 
-router.get('/monthlytransaction/:userid&:year&:month&:timezone', 
+router.get('/monthlytransactions/:userid&:year&:month&:timezone', 
 chartController.getMonthlyTransactions);
 router.get('/receipts/export/:month', usersController.exportReceipts);
 
